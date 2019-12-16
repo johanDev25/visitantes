cc135a5b795b704c88935d1e688407f1c210e247
Johan <johan225@hotmail.es>
2019-12-16 18:11:33 -0500

    Agrega opcion count

--- app.js
+++ app.js
@@ -10,20 +10,44 @@ app.use(express.urlencoded({extended: true}));
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
 mongoose.connection.on("error",function(e){console.error(e);});
 
-app.get('/', async (req, res) => {
+
+app.get('/', async (req, res, next) => {
+
   const name = (req.query.name == undefined || req.query.name == '') ? 'Anónimo' : req.query.name
-  const data ={
-    date: Date.now(),
-    name: name
+
+  if(name === 'Anónimo'){
+    const data ={
+      //date: Date.now(),
+      name: name,
+      count: 1
+    }
+    try {
+      const visitor = new Visitor(data);
+      await visitor.save();
+    } catch (e) {
+      return next(e)
+    }
+  } else {
+    const visitor = await Visitor.findOne({ name: name })
+    if(visitor){
+      visitor.count += 1;
+      try {
+        await visitor.save({});
+      } catch (e) {
+        return next(e)
+      }
+    }else{
+      const data ={ name: name, count: 1 };
+      try {
+        const visitor = new Visitor(data);
+        await visitor.save();
+      } catch (e) {
+        return next(e)
+      }
+    }
   }
-  try {
-    const visitor = new Visitor(data);
-    await visitor.save();
-    console.log(visitor)
-  } catch (e) {
-    return next(e)
-  }
-   res.send('<h1>El visitante fue almacenado con éxito</h1>');
+  const visitors = await Visitor.find();
+  res.render("index", { visitors });
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
--- models/Visitor.js
+++ models/Visitor.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-  date:Date,
-  name:String
+  //date:Date,
+  name:String,
+  count:Number
 });
 
 module.exports = mongoose.model("Visitor", schema);
